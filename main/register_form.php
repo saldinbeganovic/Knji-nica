90aec97fd719a37cb6b191e2e59e02739354f959
Saldin <64772544+saldinbeganovic@users.noreply.github.com>
2022-02-28 21:51:07 +0100

    main

---
+++ main/register_form.php
@@ -0,0 +1,22 @@
+<div class="register-form-container">
+
+    <div id="close-register-btn" class="fas fa-times"></div>
+
+    <form action="">
+        <h3>Register</h3>
+        <span>username</span>
+        <input type="email" name="" class="box" placeholder="enter your email" id="">
+        <span>username</span>
+        <input type="text" name="" class="box" placeholder="enter your username" id="">
+        <span>password</span>
+        <input type="password" name="" class="box" placeholder="enter your password" id="">
+        <div class="checkbox">
+            <input type="checkbox" name="" id="remember-me">
+            <label for="remember-me"> remember me</label>
+        </div>
+        <input type="submit" value="sign in" class="btn">
+        <p>forget password ? <a href="#">click here</a></p>
+        <p>don't have an account ? <a href="#">create one</a></p>
+    </form>
+
+</div>
